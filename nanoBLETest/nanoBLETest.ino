a0fca83b53fd73f14e2ee377e44b71913c7fde54
Michael Connelly <mrc33@atlanticbb.net>
2020-09-23 20:48:07 -0400

    device code initial update

--- nanoBLETest/nanoBLETest.ino
+++ nanoBLETest/nanoBLETest.ino
@@ -1,29 +1,32 @@
 #include <ArduinoBLE.h>
 #include <Arduino_LSM9DS1.h>
 
-String greeting = "Hello World!";
-String farewell = "Goodbye World!";
-BLEService greetingService("180C");
-BLECharacteristic greetingCharacteristic("2A56", BLERead | BLENotify, "Hello World!");
+BLEService accelerationService("180C");
+BLEService velocityService("180D");
+BLEService positionService("180E");
+BLEFloatCharacteristic accXChar("2A10", BLERead | BLENotify);
+BLEFloatCharacteristic accYChar("2A11", BLERead | BLENotify);
+BLEFloatCharacteristic accZChar("2A12", BLERead | BLENotify);
+BLEFloatCharacteristic velXChar("2A13", BLERead | BLENotify);
+BLEFloatCharacteristic velYChar("2A14", BLERead | BLENotify);
+BLEFloatCharacteristic velZChar("2A15", BLERead | BLENotify);
+BLEFloatCharacteristic posXChar("2A16", BLERead | BLENotify);
+BLEFloatCharacteristic posYChar("2A17", BLERead | BLENotify);
+BLEFloatCharacteristic posZChar("2A18", BLERead | BLENotify);
 
 float accOffsetX;
 float accOffsetY;
 float accOffsetZ;
+float velX;
+float velY;
+float velZ;
 float posX;
 float posY;
 float posZ;
+float prevMeasurementTime;
 bool first;
 
-float relativeAcceleration(float acc, float offset) {
-  if ((acc > 0.0 && offset > 0.0) || (acc < 0.0 && offset < 0.0)) {
-    return acc - offset;
-  } else {
-    return acc + offset;
-  }
-}
-
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   while (!Serial);
 
@@ -38,14 +41,38 @@ void setup() {
     while(1);
   }
 
+  accOffsetX = 0;
+  accOffsetY = 9.8;
+  accOffsetZ = 0;
+  velX = 0;
+  velY = 0;
+  velZ = 0;
+  posX = 0;
+  posY = 0;
+  posZ = 0;
+  prevMeasurementTime = 0;
   first = true;
 
   BLE.setLocalName("Nano33BLE");
-  BLE.setAdvertisedService(greetingService);
-  greetingService.addCharacteristic(greetingCharacteristic);
-  BLE.addService(greetingService);
-  /*greetingCharacteristic.setValue(greeting);*/
-
+  //BLE.setAdvertisedService(greetingService);
+  //greetingService.addCharacteristic(greetingCharacteristic);
+  //BLE.addService(greetingService);
+  BLE.setAdvertisedService(accelerationService);
+  BLE.setAdvertisedService(velocityService);
+  BLE.setAdvertisedService(positionService);
+  accelerationService.addCharacteristic(accXChar);
+  accelerationService.addCharacteristic(accYChar);
+  accelerationService.addCharacteristic(accZChar);
+  velocityService.addCharacteristic(velXChar);
+  velocityService.addCharacteristic(velYChar);
+  velocityService.addCharacteristic(velZChar);
+  positionService.addCharacteristic(posXChar);
+  positionService.addCharacteristic(posYChar);
+  positionService.addCharacteristic(posZChar);
+  BLE.addService(accelerationService);
+  BLE.addService(velocityService);
+  BLE.addService(positionService);
+  
   BLE.advertise();
   Serial.print("Peripheral device MAC: ");
   Serial.println(BLE.address());
@@ -53,34 +80,64 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   BLEDevice central = BLE.central();
-  float accX, accY, accZ, gyrX, gyrY, gyrZ, relAccX, relAccY, relAccZ gyrSample;
-  if (first) { delay(500); }
-  if (IMU.accelerationAvailable()) {
-    IMU.readAcceleration(accX, accY, accZ);
-  }
+  float accX, accY, accZ, measurementTime, dMeasurementTime;
+  float dvX, dvY, dvZ, dpX, dpY, dpZ = 0;
+  // float accsX[20], accsY[20], accsZ[20];
 
   if (central) {
-    byte greetBytes[13];
-    byte byeBytes[15];
+    if (first) { delay(500); }
     
     Serial.print("Connected to central MAC: ");
     Serial.println(central.address());
     digitalWrite(LED_BUILTIN, HIGH);
 
     while (central.connected()) {
-      farewell.getBytes(byeBytes, sizeof(byeBytes));
-      greetingCharacteristic.writeValue(byeBytes, sizeof(byeBytes));
-      delay(1000);
-      greeting.getBytes(greetBytes, sizeof(greetBytes));
-      greetingCharacteristic.writeValue(greetBytes, sizeof(greetBytes));
-      delay(1000);
+      if (IMU.accelerationAvailable()) {
+        IMU.readAcceleration(accX, accY, accZ);
+      }
+      
+      measurementTime = millis();
+      dMeasurementTime = measurementTime - prevMeasurementTime;
+
+      accX = (accX*9.8) + accOffsetX;
+      accY = (accY*9.8) + accOffsetY;
+      accZ = (accZ*9.8) + accOffsetZ;
+
+      dvX = accX * dMeasurementTime;
+      dvY = accY * dMeasurementTime;
+      dvZ = accZ * dMeasurementTime;
+
+      velX = velX + dvX;
+      velY = velY + dvY;
+      velZ = velZ + dvZ;
+
+      dpX = velX * dMeasurementTime;
+      dpY = velY * dMeasurementTime;
+      dpZ = velZ * dMeasurementTime;
+
+      posX = posX + dpX;
+      posY = posY + dpY;
+      posZ = posZ + dpZ;
+
+      prevMeasurementTime = measurementTime;
+
+      accXChar.writeValue(accX);
+      accYChar.writeValue(accY);
+      accZChar.writeValue(accZ);
+
+      velXChar.writeValue(velX);
+      velYChar.writeValue(velY);
+      velZChar.writeValue(velZ);
+
+      posXChar.writeValue(posX);
+      posYChar.writeValue(posY);
+      posZChar.writeValue(posZ);
     }
 
     digitalWrite(LED_BUILTIN, LOW);
     Serial.print("Disconnected from central MAC: ");
     Serial.println(central.address());
+    first = false;
   }
-  first = false;
 }
