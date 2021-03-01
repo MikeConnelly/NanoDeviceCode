2f9348b4e7828859b2f6847bf140c99c5e5620a8
Michael Connelly <mrc7197@gmail.com>
2021-03-01 15:40:28 -0500

    update repo

---
+++ cellularTest/cell_test/cell_test.ino
@@ -0,0 +1,31 @@
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    //wait for serial port
+  }
+
+  Serial1.begin(9600);
+}
+
+void loop() {
+  if (Serial1.available()) {
+    Serial.write(Serial1.read());
+  }
+  if (Serial.available()) {
+    Serial1.write(Serial.read());
+  }
+}
+
+// AT
+// AT+CFUN?
+// AT+SAPBR=3,1,"Contype","GBRS"
+// AT+CSTT="3gprs","3gprs","3gprs" ... this will differ by cell provider, user, password
+// AT+SAPBR=1,1
+// AT+HTTPINIT
+// AT+HTTPPARA="CID",1
+// AT+HTTPPARA="URL","http://example.com"
+// AT+HTTPACTION=0
+// AT+HTTPREAD
+// AT+HTTPTERM
+// reapeat from httpinit
